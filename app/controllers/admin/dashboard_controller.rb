2728586eb65847cb52eb0d42673eb44f469c7afd
MattSeligman <matt@modelitup.com>
2022-05-05 16:26:15 -0700

    Add @products, @total_products, and @total_categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], except: :loggedIn
   
   def show
+    @products = Product.order(id: :desc).all
+    @total_products = @products.count
+    @total_categories = @products.collect.uniq{|product| product.category.id }.count
   end
 
   def loggedIn
