fa07ae832edad134de786e4630b1145d076ed542
MattSeligman <matt@modelitup.com>
2022-05-05 14:18:39 -0700

    Add basic http_basic_authenticate_with authentication check against env

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], except: :loggedIn
+  
   def show
   end
+
+  def loggedIn
+    redirect_to '/admin/products'
+  end
+
 end
